51fa4197486d8f8c593f7cadd3cde95860a449d3
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2015-07-06 22:45:05 +1000

    angular-gridster setup

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('material', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ngMaterial']);
+    .module('material', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ngMaterial', 'gridster']);
 
 })();
